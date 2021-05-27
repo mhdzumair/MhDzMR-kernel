b563e39bfe299e4a752a3740a4a643c6e40b9682
mhdzumair <mhdzumair@gmail.com>
2021-05-27 18:33:06 +0530

    add kernel dependency

---
+++ dependency.sh
@@ -0,0 +1,2 @@
+sudo apt-get install git ccache automake lzop bison gperf build-essential zip curl zlib1g-dev g++-multilib \
+libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng
